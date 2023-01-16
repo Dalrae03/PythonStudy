98285184568f5e7f8fc34da2cdf8168b06d0b788
Dalrae03 <www298@naver.com>
2023-01-16 17:02:28 +0900

    그리디 알고리즘 두문제 풀이

--- codingtest.py
+++ codingtest.py
@@ -1,4 +1,5 @@
 # 그리디 알고리즘 - 큰 수의 법칙 (92p 실전문제)
+"""
 N, M, K = map(int, input().split())
 n = list(map(int, input().split()))
 
@@ -25,7 +26,35 @@ result += first * count
 result += second * (M-count)
 
 print(result)
+"""
 
+# 숫자 카드게임
+'''
+n, m = map(int, input().split())
+
+result = 0 #result초기화 필요
+for _ in range(n):
+    number = list(map(int, input().split()))
+    min_number = min(number)
+    result = max(min_number, result)
+
+print(result)
+# 나는 작은값들의 리스트를 만들고 그곳에서 제일 큰 수를 뽑을 걸 생각하고있엇는데.. 이럴수도 잇구만
+'''
+
+# 1이 될 때까지
+n, k = map(int, input().split())
+
+count = 0
+while n != 1:
+    if n % k == 0:
+        n = n // k
+        count += 1
+    else:
+        n -= 1
+        count += 1
+
+print(count)
 
 
 
