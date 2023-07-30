d53cfbeb4bf4fd5e8af5bad578e9438892f11cbe
Dalrae03 <www298@naver.com>
2023-07-30 17:15:51 +0900

    기출 구현 유형 1문제 '자물쇠와 열쇠' 추가

--- Pastquestion.py
+++ Pastquestion.py
@@ -352,7 +352,6 @@ print(''.join(result))
 s = 'aabbaccc'
 
 # 내 풀이 (망했고, 실패했지만... 해답의 단계와는 비슷했다...)
-
 def solution(s):
     n = 1
     l = len(s)
@@ -415,3 +414,58 @@ def solution(s):
 
 print(solution(s))
 '''
+
+
+# 10. 자물쇠와 열쇠
+# https://programmers.co.kr/learn/courses/30/lessons/60059
+'''
+key = [[0,0,0], [1,0,0], [0,1,1]]
+lock = [[1,1,1], [1,1,0], [1,0,1]]
+
+# 해답
+
+# 90도 회전 함수
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a)
+    m = len(a[0])
+    result = [[0] * n for _ in range(m)] #결과 2차원 리스트
+    for i in range(n):
+        for j in range(m):
+            result[j][n-i-1] = a[i][j]
+    return result
+
+# 자물쇠 중간 부분이 모두 1인지 확인하는 함수
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length*2):
+        for j in range(lock_length, lock_length*2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    new_lock = [[0]*(n*3) for _ in range(n*3)]
+    for i in range(n):
+        for j in range(n):
+            new_lock[i+n][j+n] = lock[i][j]
+
+    for rotation in range(4):
+        key = rotate_a_matrix_by_90_degree(key)
+        for x in range(n*2):
+            for y in range(n*2):
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x+i][y+j] += key[i][j]
+                if check(new_lock) == True:
+                    return True
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x+i][y+j] -= key[i][j]
+
+    return False
+
+print(solution(key, lock))
+'''
+
